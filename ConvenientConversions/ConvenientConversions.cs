790783a74aafc47536d25e07fbe050bbeaf17c9e
venalfax <36496472+venalfax@users.noreply.github.com>
2018-02-15 00:15:17 -0500

    Add files via upload

---
+++ ConvenientConversions/ConvenientConversions.cs
@@ -0,0 +1,25 @@
+﻿using Terraria.ModLoader;
+using ConvenientConversions.FragmentRecipes;
+
+namespace ConvenientConversions
+{
+    class ConvenientConversions : Mod
+    {
+        public ConvenientConversions()
+        {
+            Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+        public override void AddRecipes()
+        {
+            Nebula.AddRecipes(this);
+            Solar.AddRecipes(this);
+            Stardust.AddRecipes(this);
+            Vortex.AddRecipes(this);
+        }
+    }
+}
