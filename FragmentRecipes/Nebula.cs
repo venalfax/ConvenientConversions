930a14c89cd576cecbeea557dcd6f5f09d51bbb9
venalfax <venalfax@users.noreply.github.com>
2018-02-15 00:44:10 -0500

    Removed folder
    Removed redundant folder

---
+++ FragmentRecipes/Nebula.cs
@@ -0,0 +1,71 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ConvenientConversions.FragmentRecipes
+{
+    public static class Nebula
+    {
+        public static void AddRecipes(Mod mod)
+        {
+            ModRecipe S3N = new ModRecipe(mod);
+            S3N.AddIngredient(ItemID.FragmentSolar, 3);
+            S3N.AddTile(TileID.LunarCraftingStation);
+            S3N.SetResult(ItemID.FragmentNebula);
+            S3N.AddRecipe();
+
+            ModRecipe St3N = new ModRecipe(mod);
+            St3N.AddIngredient(ItemID.FragmentStardust, 3);
+            St3N.AddTile(TileID.LunarCraftingStation);
+            St3N.SetResult(ItemID.FragmentNebula);
+            St3N.AddRecipe();
+
+            ModRecipe V3N = new ModRecipe(mod);
+            V3N.AddIngredient(ItemID.FragmentVortex, 3);
+            V3N.AddTile(TileID.LunarCraftingStation);
+            V3N.SetResult(ItemID.FragmentNebula);
+            V3N.AddRecipe();
+
+            ModRecipe S2StN = new ModRecipe(mod);
+            S2StN.AddIngredient(ItemID.FragmentSolar, 2);
+            S2StN.AddIngredient(ItemID.FragmentStardust, 1);
+            S2StN.AddTile(TileID.LunarCraftingStation);
+            S2StN.SetResult(ItemID.FragmentNebula);
+            S2StN.AddRecipe();
+
+            ModRecipe S2VN = new ModRecipe(mod);
+            S2VN.AddIngredient(ItemID.FragmentSolar, 2);
+            S2VN.AddIngredient(ItemID.FragmentVortex, 1);
+            S2VN.AddTile(TileID.LunarCraftingStation);
+            S2VN.SetResult(ItemID.FragmentNebula);
+            S2VN.AddRecipe();
+
+            ModRecipe St2SN = new ModRecipe(mod);
+            St2SN.AddIngredient(ItemID.FragmentStardust, 2);
+            St2SN.AddIngredient(ItemID.FragmentSolar, 1);
+            St2SN.AddTile(TileID.LunarCraftingStation);
+            St2SN.SetResult(ItemID.FragmentNebula);
+            St2SN.AddRecipe();
+
+            ModRecipe St2VN = new ModRecipe(mod);
+            St2VN.AddIngredient(ItemID.FragmentStardust, 2);
+            St2VN.AddIngredient(ItemID.FragmentVortex, 1);
+            St2VN.AddTile(TileID.LunarCraftingStation);
+            St2VN.SetResult(ItemID.FragmentNebula);
+            St2VN.AddRecipe();
+
+            ModRecipe V2StN = new ModRecipe(mod);
+            V2StN.AddIngredient(ItemID.FragmentVortex, 2);
+            V2StN.AddIngredient(ItemID.FragmentStardust, 1);
+            V2StN.AddTile(TileID.LunarCraftingStation);
+            V2StN.SetResult(ItemID.FragmentNebula);
+            V2StN.AddRecipe();
+
+            ModRecipe V2SN = new ModRecipe(mod);
+            V2SN.AddIngredient(ItemID.FragmentVortex, 2);
+            V2SN.AddIngredient(ItemID.FragmentSolar, 1);
+            V2SN.AddTile(TileID.LunarCraftingStation);
+            V2SN.SetResult(ItemID.FragmentNebula);
+            V2SN.AddRecipe();
+        }
+    }
+}
---
+++ FragmentRecipes/Solar.cs
@@ -0,0 +1,71 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ConvenientConversions.FragmentRecipes
+{
+    public static class Solar
+    {
+        public static void AddRecipes(Mod mod)
+        {
+            ModRecipe N3S = new ModRecipe(mod);
+            N3S.AddIngredient(ItemID.FragmentNebula, 3);
+            N3S.AddTile(TileID.LunarCraftingStation);
+            N3S.SetResult(ItemID.FragmentSolar);
+            N3S.AddRecipe();
+
+            ModRecipe St3S = new ModRecipe(mod);
+            St3S.AddIngredient(ItemID.FragmentStardust, 3);
+            St3S.AddTile(TileID.LunarCraftingStation);
+            St3S.SetResult(ItemID.FragmentSolar);
+            St3S.AddRecipe();
+
+            ModRecipe V3S = new ModRecipe(mod);
+            V3S.AddIngredient(ItemID.FragmentVortex, 3);
+            V3S.AddTile(TileID.LunarCraftingStation);
+            V3S.SetResult(ItemID.FragmentSolar);
+            V3S.AddRecipe();
+
+            ModRecipe N2StS = new ModRecipe(mod);
+            N2StS.AddIngredient(ItemID.FragmentNebula, 2);
+            N2StS.AddIngredient(ItemID.FragmentStardust, 1);
+            N2StS.AddTile(TileID.LunarCraftingStation);
+            N2StS.SetResult(ItemID.FragmentSolar);
+            N2StS.AddRecipe();
+
+            ModRecipe N2VS = new ModRecipe(mod);
+            N2VS.AddIngredient(ItemID.FragmentNebula, 2);
+            N2VS.AddIngredient(ItemID.FragmentVortex, 1);
+            N2VS.AddTile(TileID.LunarCraftingStation);
+            N2VS.SetResult(ItemID.FragmentSolar);
+            N2VS.AddRecipe();
+
+            ModRecipe St2NS = new ModRecipe(mod);
+            St2NS.AddIngredient(ItemID.FragmentStardust, 2);
+            St2NS.AddIngredient(ItemID.FragmentNebula, 1);
+            St2NS.AddTile(TileID.LunarCraftingStation);
+            St2NS.SetResult(ItemID.FragmentSolar);
+            St2NS.AddRecipe();
+
+            ModRecipe St2VS = new ModRecipe(mod);
+            St2VS.AddIngredient(ItemID.FragmentStardust, 2);
+            St2VS.AddIngredient(ItemID.FragmentVortex, 1);
+            St2VS.AddTile(TileID.LunarCraftingStation);
+            St2VS.SetResult(ItemID.FragmentSolar);
+            St2VS.AddRecipe();
+
+            ModRecipe V2StS = new ModRecipe(mod);
+            V2StS.AddIngredient(ItemID.FragmentVortex, 2);
+            V2StS.AddIngredient(ItemID.FragmentStardust, 1);
+            V2StS.AddTile(TileID.LunarCraftingStation);
+            V2StS.SetResult(ItemID.FragmentSolar);
+            V2StS.AddRecipe();
+
+            ModRecipe V2NS = new ModRecipe(mod);
+            V2NS.AddIngredient(ItemID.FragmentVortex, 2);
+            V2NS.AddIngredient(ItemID.FragmentNebula, 1);
+            V2NS.AddTile(TileID.LunarCraftingStation);
+            V2NS.SetResult(ItemID.FragmentSolar);
+            V2NS.AddRecipe();
+        }
+    }
+}
